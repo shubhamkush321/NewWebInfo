0db4649a458755a0f8c610a50035c6e6192bdc17
shubham <142429585+shubhamkush321@users.noreply.github.com>
2024-08-31 08:07:36 +0530

    Form And Search Functionality Add

--- src/components/Admin/Form.jsx
+++ src/components/Admin/Form.jsx
@@ -26,10 +26,10 @@ const Form = ({ setFormData, formData }) => {
     <div className='flex'>
     <div className=" mt-20 min-h-screen p-6">
       {/* Main form section */}
-      <form onSubmit={handleFormSubmit} className="w-full max-w-6xl mx-auto bg-white bg-opacity-30 backdrop-blur-md p-6 rounded-lg shadow-lg h-full max-h-3xl">
+      <form onSubmit={handleFormSubmit} className="w-full max-w-6xl mx-auto bg-white bg-opacity-50 backdrop-blur-md p-6 rounded-lg shadow-lg h-full max-h-3xl">
         <h1 className="text-2xl font-bold mb-6 text-gray-900">Edit Form</h1>
         <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2" htmlFor="name">
+          <label className="block text-xl text-gray-800 font-semibold mb-2" htmlFor="name">
             Name
           </label>
           <input
@@ -37,12 +37,12 @@ const Form = ({ setFormData, formData }) => {
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="relative w-full px-4 py-2 border border-blue-300 bg-white rounded-md text-gray-800 font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2" htmlFor="permalink">
+          <label className="block text-xl text-gray-800 font-semibold mb-2" htmlFor="permalink">
             Permalink
           </label>
           <input
@@ -50,13 +50,13 @@ const Form = ({ setFormData, formData }) => {
             type="text"
             value={permalink}
             onChange={(e) => setPermalink(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="relative w-full px-4 py-2 border border-blue-300 bg-white rounded-md text-gray-800 font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <small className="text-gray-500">Preview: https://www.example.com/{permalink}</small>
+          <small className="text-gray-500 text-xl">Preview: https://www.example.com/{permalink}</small>
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2" htmlFor="description">
+          <label className="block text-xl text-gray-800 font-semibold mb-2" htmlFor="description">
             Description
           </label>
           <textarea
@@ -64,12 +64,12 @@ const Form = ({ setFormData, formData }) => {
             rows="4"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="relative w-full px-4 py-2 border border-blue-300 bg-white rounded-md text-gray-800 font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500"
           ></textarea>
         </div>
 
         <div className="mb-4">
-          <label className="block text-gray-700 font-semibold mb-2">
+          <label className="block text-xl text-gray-800 font-semibold mb-2">
             Content
           </label>
           <JoditEditor
@@ -79,7 +79,7 @@ const Form = ({ setFormData, formData }) => {
             config={{
               readonly: false,
             }}
-            className="border border-gray-300 rounded-md bg-transparent text-gray-800"
+            className="rounded-md bg-transparent text-gray-900"
           />
         </div>
         
@@ -100,55 +100,56 @@ const Form = ({ setFormData, formData }) => {
       </form>
     </div>
     {/* Side section for Status and Template */}
-    <div className="w-68 flex flex-col gap-4">
-    <div className="p-6 bg-white bg-opacity-30 backdrop-blur-md shadow-lg rounded-lg">
-      <label className="block text-gray-700 font-semibold mb-2" htmlFor="status">
+    <div className="w-96 flex flex-col gap-4 mt-1">
+    <div className="p-6 bg-white bg-opacity-50 backdrop-blur-md shadow-lg rounded-lg">
+      <label className="block text-xl text-gray-800 font-semibold mb-2" htmlFor="status">
         Publish
       </label>
-      <hr className="border-gray-300 mb-4" />
+      <hr className="border-gray-500 mb-4" />
       <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 mb-2">Save</button>
       <br />
       <button className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600">Save & Exit</button>
     </div>
 
-    <div className="p-8 bg-white bg-opacity-30 backdrop-blur-md shadow-lg rounded-lg">
-      <label className="block text-gray-700 font-semibold mb-2" htmlFor="status">
+    <div className="p-8 bg-white bg-opacity-50 backdrop-blur-md shadow-lg rounded-lg">
+      <label className="block text-xl text-gray-700 font-semibold mb-2" htmlFor="status">
         Status
       </label>
-      <hr className="border-gray-300 mb-4" />
+      <hr className="border-gray-500 mb-4" />
       <select
-        id="status"
-        value={status}
-        onChange={(e) => setStatus(e.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      >
-        <option value="published">Published</option>
-        <option value="draft">Draft</option>
-        <option value="pending">Pending</option>
-      </select>
+  id="status"
+  value={status}
+  onChange={(e) => setStatus(e.target.value)}
+  className="relative w-full px-4 py-2 border border-blue-300 bg-white rounded-md text-gray-800 font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500"
+>
+  {/* Custom Arrow */}
+  <option value="published">Published</option>
+  <option value="draft">Draft</option>
+  <option value="pending">Pending</option>
+</select>
     </div>
 
-    <div className="p-8 bg-white bg-opacity-30 backdrop-blur-md shadow-lg rounded-lg">
-      <label className="block text-gray-700 font-semibold mb-2" htmlFor="template">
+    <div className="p-8 bg-white bg-opacity-50 backdrop-blur-md shadow-lg rounded-lg">
+      <label className="block text-xl text-gray-700 font-semibold mb-2" htmlFor="template">
         Template
       </label>
-      <hr className="border-gray-300 mb-4" />
+      <hr className="border-gray-500 mb-4" />
       <select
         id="template"
         value={template}
         onChange={(e) => setTemplate(e.target.value)}
-        className="w-full px-3 py-2 border border-gray-300 rounded-md bg-transparent text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="relative w-full px-4 py-2 border border-blue-300 bg-white rounded-md text-gray-800 font-semibold focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         <option value="default">Full width</option>
         <option value="custom">HomePage</option>
       </select>
     </div>
 
-    <div className="p-8 bg-white bg-opacity-30 backdrop-blur-md shadow-lg rounded-lg">
-      <label className="block text-gray-700 font-semibold mb-2" htmlFor="template">
+    <div className="p-8 bg-white bg-opacity-50 backdrop-blur-md shadow-lg rounded-lg">
+      <label className="block text-xl text-gray-700 font-semibold mb-2" htmlFor="template">
         Image
       </label>
-      <hr className="border-gray-300 mb-4" />
+      <hr className="border-gray-500 mb-4" />
 
       <div className="flex items-center justify-center space-x-4">
         <button
