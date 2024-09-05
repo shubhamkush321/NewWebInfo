4eec3cfefd453c6c2d6e941f874f22a9380acde2
shubham <142429585+shubhamkush321@users.noreply.github.com>
2024-09-05 07:21:12 +0530

    Button or Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -55,15 +55,16 @@ const animationVariants = {
 
 const Home = () => {
   const { infoDetails } = useContext(InfoContext);
-  const HomePageTitle = infoDetails?.find(
-    (item) => item?.title === "Home Page Title"
+  const HomeInfo = infoDetails?.find(
+    (item) => item?.items[0].title === "Professional Digital"
   );
 
   const controls = useAnimation();
   const textControls = useAnimation();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [initialLoad, setInitialLoad] = useState(true);
-  const slideInterval = 5000; 
+  const slideInterval = 5000;
+
   const handleSlideChange = () => {
     controls.start("exit").then(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
@@ -84,6 +85,7 @@ const Home = () => {
         });
       });
   };
+
   useEffect(() => {
     if (initialLoad) {
       setTimeout(() => {
@@ -99,13 +101,14 @@ const Home = () => {
       return () => clearInterval(interval);
     }
   }, [initialLoad, controls, textControls]);
+
   return (
     <LocomotiveScrollProvider options={{ smooth: true }} watch={[]}>
       <div data-scroll-container className="relative overflow-hidden h-screen w-screen">
         {slides.map((slide, index) => (
           <motion.div
             key={index}
-            className="absolute top-0 left-0 w-screen h-screen"
+            className="absolute inset-0 w-full h-full"
             variants={variants}
             initial={index === 0 ? "visible" : "hidden"}
             animate={controls}
@@ -127,9 +130,11 @@ const Home = () => {
                 slide.textPosition === "right" ? "right-6" : "left-6"
               } top-1/2 transform -translate-y-1/2 p-6 text-white z-10`}
             >
-              <h2 className="text-7xl font-bold mb-4 text-gray-50 -mt-10">{slide.heading}</h2>
+              <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4 text-gray-50 -mt-10">
+                {slide.heading}
+              </h2>
               {slide.paragraphs.map((text, i) => (
-                <p key={i} className="text-lg mb-2 text-gray-50">
+                <p key={i} className="text-base md:text-lg mb-2 text-gray-50">
                   {text}
                 </p>
               ))}
@@ -140,12 +145,12 @@ const Home = () => {
           </motion.div>
         ))}
       </div>
-      {HomePageTitle && (
-        <div className="lg:p-44 text-gray-800 bg-gray-100 -mt-20">
-          <h1 className="text-3xl font-bold mb-6 text-gray-800 lg:text-center uppercase">
-            {HomePageTitle.title}
+      {HomeInfo && (
+        <div className="text-gray-800 w-full px-4 lg:px-scale-90 py-10">
+          <h1 className="text-xl md:text-2xl lg:text-2xl xl:text-4xl  text-gray-800 lg:text-center uppercase">
+            {HomeInfo?.items[0]?.title} <span className="text-blue-500">Marketing Services</span>
           </h1>
-          <hr className="border-gray-500 mb-4" />
+          <hr className="border-gray-800 mb-4 w-20 mx-auto border-2"/>
           <div className="mb-8">
             <motion.div
               initial="hiddenTop"
@@ -153,20 +158,18 @@ const Home = () => {
               viewport={{ once: true }}
               transition={{ duration: 0.8, ease: "easeOut" }}
               variants={animationVariants}
-              className="relative z-10 backdrop-filter rounded-xl p-10"
+              className="relative z-10 backdrop-filter bg-opacity-50 rounded-xl p-6 lg:p-10"
             >
               <div
-                className="mb-6 text-gray-700"
+                className="text-gray-700"
                 dangerouslySetInnerHTML={{
-                  __html: HomePageTitle.content?.map(section => section.content).join('<br/><br/>') || "",
+                  __html: HomeInfo?.items[0]?.content,
                 }}
               />
             </motion.div>
           </div>
         </div>
       )}
-
-      
     </LocomotiveScrollProvider>
   );
 };
